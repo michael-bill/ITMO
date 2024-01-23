d9db4ca0fa338503e774507c95d5464458192696
Michael-Bill <biloshitskiy_m@mail.ru>
2024-01-23 14:59:30 +0300

    Update user controller

--- Sem 3/Веб/laba4/laba4_back/src/main/java/org/michael/laba4_back/controller/UserController.java
+++ Sem 3/Веб/laba4/laba4_back/src/main/java/org/michael/laba4_back/controller/UserController.java
@@ -7,6 +7,8 @@ import org.michael.laba4_back.repository.UserRepository;
 import static org.michael.laba4_back.Utils.getRandomHash;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,46 +27,49 @@ public class UserController {
     private AuthTokenRepository authTokenRepository;
 
     @PostMapping(value = "/user-reg", produces = "application/json")
-    public String addUser(@RequestParam String login, @RequestParam String pass) throws NoSuchAlgorithmException {
+    public ResponseEntity<String> addUser(@RequestParam String login, @RequestParam String pass) throws NoSuchAlgorithmException {
         int LOGIN_LENGTH = 4;
         int PASS_LENGTH = 8;
         if (login.length() < LOGIN_LENGTH) {
-            return getMessage("Login needs to be at least " + LOGIN_LENGTH + " characters");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(getMessage("Login needs to be at least " + LOGIN_LENGTH + " characters"));
         } else if (pass.length() < PASS_LENGTH) {
-            return getMessage("Password needs to be at least " + PASS_LENGTH + " characters");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(getMessage("Password needs to be at least " + PASS_LENGTH + " characters"));
         } else if (userRepository.findByLogin(login) != null) {
-            return getMessage("User with this login already exists");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(getMessage("User with this login already exists"));
         }
         String hashEdPass = sha256(pass);
         userRepository.save(new User(login, hashEdPass));
-        return getMessage("User created");
+        return ResponseEntity.ok(getMessage("User created"));
     }
 
     @GetMapping(value = "/user-auth", produces = "application/json")
-    public String authUser(@RequestParam String login, @RequestParam String pass) throws NoSuchAlgorithmException {
+    public ResponseEntity<String> authUser(@RequestParam String login, @RequestParam String pass) throws NoSuchAlgorithmException {
         User user = userRepository.findByLogin(login);
         if (user == null) {
-            return getMessage("User not found");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(getMessage("User not found"));
         }
         String hashEdPass = sha256(pass);
         if (!hashEdPass.equals(user.getPass())) {
-            return getMessage("Wrong password");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(getMessage("Wrong password"));
         }
         if (authTokenRepository.findByUser(user) != null) {
             authTokenRepository.deleteByUserId(user.getId());
         }
         String token = getRandomHash();
         authTokenRepository.save(new AuthToken(user, token, LocalDateTime.now()));
-        return getMessage("token", token);
+        return ResponseEntity.ok(getMessage("token", token));
     }
 
     @GetMapping(value = "/user-logout", produces = "application/json")
-    public String logoutUser(@RequestParam String authToken) {
+    public ResponseEntity<String> logoutUser(@RequestParam String authToken) {
         AuthToken token = authTokenRepository.findByToken(authToken);
         if (token == null) {
-            return getMessage("Wrong token");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(getMessage("Wrong token"));
         }
         authTokenRepository.deleteByUserId(token.getUser().getId());
-        return getMessage("User logged out");
+        return ResponseEntity.ok(getMessage("User logged out"));
     }
 }
